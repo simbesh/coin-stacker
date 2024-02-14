f0a8a00c36fd44c750f295c62d221fddcae01e11
Simon Bechard <simon.bechard@gmail.com>
2024-02-14 11:12:50 +1000

    fix build error

--- src/exchange-config.ts
+++ src/exchange-config.ts
@@ -14,7 +14,6 @@ const EXCHANGE_URLS: Record<string, string> = {
     swyftx: 'https://www.swyftx.com',
     coinstash: 'https://www.coinstash.com.au',
     cointree: 'https://www.cointree.com',
-    digitalsurge: 'https://www.digitalsurge.com.au',
 }
 const ALT_EXCHANGE_URLS: Record<string, string> = {
     coinjar: 'https://exchange.coinjar.com/assets/favicons/favicon.ico',
