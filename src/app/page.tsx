f0ee797e4925c16a0e230cc2a5d548908659d838
Simon Bechard <simon.bechard@gmail.com>
2024-02-14 13:28:08 +1000

    fix title text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
                 <h3 className="text-center text-5xl font-bold">
                     <span className={'text-3xl'}>🇦🇺 </span>Crypto Price Aggregator
                 </h3>
-                <h2 className="mt-2 flex flex-wrap items-end justify-center gap-1 text-3xl font-bold text-slate-400 dark:text-slate-500">
+                <h2 className="mt-2 flex flex-wrap items-end justify-center gap-1 text-3xl font-bold text-slate-600 dark:text-slate-200">
                     <div className={'whitespace-nowrap'}>Find the best price...</div>
                     <div className="from-primary bg-gradient-to-l to-slate-600 bg-clip-text text-transparent dark:to-slate-200">
                         Stack more coins!
